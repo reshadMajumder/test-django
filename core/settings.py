6f7417f6ec036adcd8978f2ae035eada52664ae1
Jahidul Hassan Reshad <127494557+reshadMajumder@users.noreply.github.com>
2025-05-31 15:09:01 +0600

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3edv^yxnz+v6facob8rv7+d$^1f=m#-tbh2+l*!rgs%xdtuyaj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
